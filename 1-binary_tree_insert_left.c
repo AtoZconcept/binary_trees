0486910439d480129a79e58be39415fc5bfd2ac3
AtoZconcept <opeyemiolaafeez.com>
2024-01-04 15:32:42 +0100

    betyy

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,8 @@
  * @parent: pointer to the node to insert the left-child in
  * @value: the value to store in the new node
  *
- * Return:  pointer to the created node, or NULL on failure or if parent is NULL
+ * Return:  pointer to the created node, or NULL on failure or
+ * if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
