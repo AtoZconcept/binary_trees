ac576ff08c443d4a082b9536abac941e4e309356
AtoZconcept <opeyemiolaafeez.com>
2024-01-05 13:21:21 +0100

    add

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function that inserts a node
+ * binary_tree_insert_right - function that inserts a node
  * as the right-child of another node
  *
  * @parent: pointer to the node to insert the righ-child in
